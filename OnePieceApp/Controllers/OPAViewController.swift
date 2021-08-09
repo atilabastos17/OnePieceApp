b6b724b68de4faeb3f21e5609a87156f2aee339e
atilafb <atilafb77@gmail.com>
2021-08-09 02:41:38 -0300

    Delete the labels and add the tableview

--- OnePieceApp/Controllers/OPAViewController.swift
+++ OnePieceApp/Controllers/OPAViewController.swift
@@ -10,20 +10,12 @@ import UIKit
 class OPAViewController: UIViewController {
     
     @IBOutlet weak var searchTextField: UITextField!
-    @IBOutlet weak var chapterNumberLabel: UILabel!
-    @IBOutlet weak var chapterTitleLabel: UILabel!
-    @IBOutlet weak var summaryLabel: UILabel!
-    @IBOutlet weak var charactersLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     var opaManager = OPAManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        chapterNumberLabel.alpha = 0.5
-        chapterTitleLabel.alpha = 0.5
-        summaryLabel.alpha = 0.5
-        charactersLabel.alpha = 0.5
         
         searchTextField.delegate = self
         opaManager.delegate = self
@@ -37,13 +29,6 @@ extension OPAViewController: UITextFieldDelegate {
     
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
-        chapterNumberLabel.alpha = 1
-        chapterTitleLabel.alpha = 1
-        summaryLabel.alpha = 1
-        charactersLabel.alpha = 1
-        
-        summaryLabel.numberOfLines = 0
-        charactersLabel.numberOfLines = 0
         
     }
     
@@ -77,10 +62,10 @@ extension OPAViewController: OPAManagerDelegate {
     
     func didUpdateChapter(_ opaManager: OPAManager, chapterNumberInfo: OPAModel) {
         DispatchQueue.main.async {
-            self.chapterNumberLabel.text = chapterNumberInfo.chapter
-            self.chapterTitleLabel.text = chapterNumberInfo.title
-            self.summaryLabel.text = chapterNumberInfo.summary
-            self.charactersLabel.text = chapterNumberInfo.characters
+//            self.chapterNumberLabel.text = chapterNumberInfo.chapter
+//            self.chapterTitleLabel.text = chapterNumberInfo.title
+//            self.summaryLabel.text = chapterNumberInfo.summary
+//            self.charactersLabel.text = chapterNumberInfo.characters
         }
 
     }
