0223740daf657719ba38cfd888b39e78b4959a33
atilafb <atilafb77@gmail.com>
2021-08-23 04:13:49 -0300

    Fix the performSegue problem, now the app load the correct information after the user's input

--- OnePieceApp/Controllers/OPASearchViewController.swift
+++ OnePieceApp/Controllers/OPASearchViewController.swift
@@ -24,14 +24,12 @@ class OPASearchViewController: UIViewController {
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
         
-        
         // Checking if the input is valid
         if searchTextField.text != "" {
             if let chapter = searchTextField.text {
                 opaManager.getChapterNumber(for: chapter)
-                performSegue(withIdentifier: "searchToSummary", sender: self)
             }
-            
+            // Changing the placeholder text after a succesful search
             searchTextField.placeholder = "Enter a chapter number"
         } else {
             searchTextField.placeholder = "You must type a chapter number"
@@ -61,6 +59,11 @@ extension OPASearchViewController: OPAManagerDelegate {
         let newChapterInfo = OPAModel(chapter: chapterNumberInfo.chapter, title: chapterNumberInfo.title, summary: chapterNumberInfo.summary, characters: chapterNumberInfo.characters)
         
         model = newChapterInfo
+        
+        // Making the performSegue after the API request is done and my model is filled with the data from the request
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "searchToSummary", sender: self)
+        }
     }
     
     func didFailWithError(_ opaManager: OPAManager, error: Error) {
